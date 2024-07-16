faf0cc3c024a411ded78796ce29db3bbb18513ea
Muzammil Ali <muzammilali28@gmail.com>
2024-07-16 23:43:27 +0500

    test implementation of spin the wheel for Doggs App @ DuoLabz

--- app/page.jsx
+++ app/page.jsx
@@ -16,6 +16,10 @@ export default function Home() {
       <HorizontalScrollCarousel />
       <MultiLayerParallax />
       <AnimateSVG />
+      <div className="bg-red-200 flex items-center justify-center relative">
+        <img src="/wheel.svg" alt="wheel-svg" className="z-[1] inset-0 rotate-0 hover:rotate-180 transition duration-1000 origin-center" />
+        <img src="/spin-wheel-center-image.jpeg" alt="spin-wheel-center-image" className="absolute w-[340px] z-[0]"/>
+      </div>
     </main>
   );
 }
